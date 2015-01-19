3a76e9e2bb15bdbac96a3a6846152c694403fb36
daweibalong <daweibalong@gmail.com>
2015-01-19 19:41:27 +0800

    格式

--- cond.c
+++ cond.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define cond_begin 				do{
-#define cond_end   				}while(0);
-#define cond_case(co, action) 	if(co) {action;break;}
-#define cond_else(action) 		action;
+#define cond_begin              do{
+#define cond_end                }while(0);
+#define cond_case(co, action)   if(co) {action;break;}
+#define cond_else(action)       action;
 
 int main()
 {
@@ -15,11 +15,11 @@ int main()
 		scanf("%d", &a);
 		cond_begin
 			cond_case(a == 0, printf("%d\n", a);
-							  printf("a==0\n");)
+                              printf("a==0\n");)
 			cond_case(a == 1, printf("%d\n", a);
-							  printf("a==1\n");)
+                              printf("a==1\n");)
 			cond_case(a == 2, printf("a==2\n");
-					  		  printf("default2\n"))
+                              printf("default2\n"))
 			cond_else(break;)
 		cond_end
 	}	
